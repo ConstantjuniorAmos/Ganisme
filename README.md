72ad41ee6ee37b052d2d0a8ce1c1379cbdbc5e0f
mathieu-duverne <mathieu.duverne@laplateforme.io>
2023-01-24 19:28:13 +0100

    preprocess & dcgan 64 - 128

---
+++ README.md
@@ -0,0 +1,18 @@
+# GAN (Generative Adversial Network)
+
+## PB : Génération d'images à partir de peintures
+
+le jeux de donnée utilisé : https://docs.google.com/spreadsheets/d/1zP0jnTrZiTbAkwuoww8XA5_JlLD4vSiH/edit?usp=sharing&ouid=103541044914184994509&rtpof=true&sd=true
+
+### 1 : Scrapp des images de `TYPE` painting 
+
+### 2 : Data analyse
+
+### 3 : pre traitements du jeu de donnée d'images
+    - preprocess_img-data : 
+
+### 4 : DCGAN ( Deep Convolutional ...)
+
+    - dcgan_64x64 :
+
+    - dcgan_128x128 : 
---
+++ dcgan_128x128/train.py
@@ -0,0 +1,140 @@
+"""
+Training of DCGAN network on MNIST dataset with Discriminator
+and Generator imported from models.py
+Programmed by Aladdin Persson <aladdin.persson at hotmail dot com>
+* 2020-11-01: Initial coding
+* 2022-12-20: Small revision of code, checked that it works with latest PyTorch version
+"""
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+# from torch.utils.tensorboard import SummaryWriter
+# from tensorboard.plugins.hparams import api as hp
+from model import Discriminator, Generator, initialize_weights
+
+
+
+# Hyperparameters etc.
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+LEARNING_RATE = 2e-4  # could also use two lrs, one for gen and one for disc
+BATCH_SIZE = 32
+IMAGE_SIZE = 128
+CHANNELS_IMG = 3
+NOISE_DIM = 100
+NUM_EPOCHS = 15
+FEATURES_DISC = 128
+FEATURES_GEN = 128
+
+transforms = transforms.Compose(
+    [
+        transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            [0.5 for _ in range(CHANNELS_IMG)], [0.5 for _ in range(CHANNELS_IMG)]
+        ),
+    ]
+)
+
+dataset = datasets.ImageFolder(root="../data_dcgan", transform=transforms)
+
+dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
+
+gen = Generator(NOISE_DIM, CHANNELS_IMG, FEATURES_GEN).to(device)
+disc = Discriminator(CHANNELS_IMG, FEATURES_DISC).to(device)
+
+initialize_weights(gen)
+initialize_weights(disc)
+
+opt_gen = optim.Adam(gen.parameters(), lr=LEARNING_RATE, betas=(0.5, 0.999))
+
+opt_disc = optim.Adam(disc.parameters(), lr=LEARNING_RATE, betas=(0.5, 0.999))
+
+criterion = nn.BCELoss()
+
+fixed_noise = torch.randn(32, NOISE_DIM, 1, 1).to(device)
+# writer_real = SummaryWriter(f"logs/real")
+# writer_fake = SummaryWriter(f"logs/fake")
+
+step = 0
+
+gen.train()
+disc.train()
+G_losses = []
+D_losses = []
+
+for epoch in range(NUM_EPOCHS):
+    # Target labels not needed! <3 unsupervised
+    for batch_idx, (real, _) in enumerate(dataloader):
+        real = real.to(device)
+        noise = torch.randn(BATCH_SIZE, NOISE_DIM, 1, 1).to(device)
+        fake = gen(noise)
+
+        ### Train Discriminator: max log(D(x)) + log(1 - D(G(z)))
+        disc_real = disc(real).reshape(-1)
+        loss_disc_real = criterion(disc_real, torch.ones_like(disc_real))
+        disc_fake = disc(fake.detach()).reshape(-1)
+        loss_disc_fake = criterion(disc_fake, torch.zeros_like(disc_fake))
+        loss_disc = (loss_disc_real + loss_disc_fake) / 2
+        disc.zero_grad()
+        loss_disc.backward()
+        opt_disc.step()
+
+        ### Train Generator: min log(1 - D(G(z))) <-> max log(D(G(z))
+        output = disc(fake).reshape(-1)
+        loss_gen = criterion(output, torch.ones_like(output))
+        gen.zero_grad()
+        loss_gen.backward()
+        opt_gen.step()
+
+        # Print losses occasionally and print to tensorboard
+        if batch_idx % 100 == 0:
+            print(
+                f"Epoch [{epoch}/{NUM_EPOCHS}] Batch {batch_idx}/{len(dataloader)} \
+                  Loss D: {loss_disc:.4f}, loss G: {loss_gen:.4f}"
+            )
+
+            # loss_g = f"{loss_gen:.4f}"
+            # loss_d = f"{loss_disc:.4f}"
+            
+            i = 0
+            for r in real:
+
+                torchvision.utils.save_image(real[i], f"real/real_{step}_{i}.jpg", normalize=True)
+                torchvision.utils.save_image(fake[i], f"fake/fake_{step}_{i}.jpg", normalize=True)
+                i += 1
+
+            # writer_fake.add_scalar("loss G", float(loss_g), step)
+            # writer_real.add_scalar("loss D", float(loss_d), step)
+
+            with torch.no_grad():
+                fake = gen(fixed_noise)
+                # take out (up to) 32 examples
+                img_grid_real = torchvision.utils.make_grid(real[:32], normalize=True)
+                img_grid_fake = torchvision.utils.make_grid(fake[:32], normalize=True)
+
+                # writer_real.add_image("Real", img_grid_real, global_step=step)
+                # writer_fake.add_image("Fake", img_grid_fake, global_step=step)
+            
+            G_losses.append(loss_gen.item())
+            D_losses.append(loss_disc.item())
+            step += 1
+            
+    # vizualize model
+    # writer_fake.add_graph(gen, input_to_model=noise)
+    # writer_real.add_graph(disc, input_to_model=real)
+
+    print(" ------------ Fin d'entrainements ------------ ")
+
+plt.figure(figsize=(10,5))
+plt.title("Generator and Discriminator Loss During Training")
+plt.plot(G_losses,label="G")
+plt.plot(D_losses,label="D")
+plt.xlabel("iterations")
+plt.ylabel("Loss")
+plt.legend()
+plt.show()
